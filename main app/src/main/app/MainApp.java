3ca0d084c0ea3a9bc65f80d9833d44ec776a3d86
Menaka Anton <menakaantonjayakody@gmail.com>
2023-05-15 10:18:26 +0530

    message added

--- main app/src/main/app/MainApp.java
+++ main app/src/main/app/MainApp.java
@@ -14,8 +14,8 @@ public class MainApp {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        System.out.println("Hello Diluka");
+        System.out.println("Hello Diluka!");
         // TODO code application logic here
     }
-    
+
 }
